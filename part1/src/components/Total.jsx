da197eeac314dd2ae1975891b26e80d1ccd46070
sangamesh77 <sangameshvenu@gmail.com>
2024-09-22 09:58:27 +0530

    reset

--- part1/src/components/Total.jsx
+++
@@ -1,9 +0,0 @@
-import { ExerciseNums } from "../assets/Constants"
-
-const Total = () => {
-    return (
-        <p>Number of exercises = {ExerciseNums.exercises1 + ExerciseNums.exercises2 + ExerciseNums.exercises3}</p>
-    )
-}
-
-export default Total
