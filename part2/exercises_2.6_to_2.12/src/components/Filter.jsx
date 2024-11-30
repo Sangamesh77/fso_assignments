f2831c06ee1885cea6bee12387e2ef8864abe2b9
sangameshvenu <106668092+sangameshvenu@users.noreply.github.com>
2024-11-30 11:27:32 +0530

    .

--- part2/exercises_2.6_to_2.12/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({ filterString, filterEventHandler }) => {
-    return(
-        <div>
-            filter shown with: <input value={filterString} onChange={filterEventHandler}/>
-        </div>
-    )
-}
-
-export default Filter
