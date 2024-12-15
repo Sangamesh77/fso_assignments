9afcd1dd9709b1b6b70cd6efd2d2d42b812b9001
Sangameshwar <sangameshwar.venugopal@gmail.com>
2024-12-15 15:01:45 +0530

    test

--- part3/exercises_3.1_to_3.6/index.js
+++ part3/exercises_3.1_to_3.6/index.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require('cors')
 const app = express()
 morgan.token("payload", (request) => JSON.stringify(request.body))
-
+app.use(cors())
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :payload"))
 
@@ -82,6 +83,6 @@ app.post("/api/persons", (request, response) => {
     }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
